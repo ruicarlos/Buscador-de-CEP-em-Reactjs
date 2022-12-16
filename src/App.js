df5fb7349488cfdd5d00971c5514e8e385e65b28
ruicarlos <Ruicarlos3g.rc@gmail.com>
2022-12-15 23:22:04 -0300

    Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import {FiSearch} from 'react-icons/fi';
+import { useState } from 'react';
+import './styles.css';
+
+import api from './services/api';
 
 function App() {
+
+  const [cepInformado,setcepInformado] =useState('')
+  const [cep,setcep] =useState({})
+
+  async function searchCep (){
+   if(cepInformado ===''){
+    alert("informe um cep")
+    return;
+   }
+   try{
+    const response = await api.get(`${cepInformado}/json`);
+    setcep(response.data);
+    setcepInformado("");
+   }catch{
+    alert('Tente Novamente');
+    setcepInformado("")
+   }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Busca CEP</h1>
+
+      <div className="containerInput">
+        <input 
+          type="text"
+          placeholder="Digite o CEP..."
+          value={cepInformado}
+          onChange={(e)=> setcepInformado(e.target.value)}
+        />
+
+
+        <button className="buttonSearch" onClick={searchCep}>
+          <FiSearch size={25} color='#FFF'/>
+        </button>
+      </div>
+
+      <main className='main'>
+        <h2>CEP: {cep.cep}</h2>
+        <span>Logradouro: {cep.logradouro}</span>
+        <span>Complemento: {cep.complemento}</span>
+        <span>Bairro: {cep.bairro}</span>
+        <span>Cidade: {cep.localidade}</span>
+        <span>DDD: {cep.ddd}</span>
+        <span>UF: {cep.uf}</span>
+
+      </main>
+
     </div>
   );
 }
